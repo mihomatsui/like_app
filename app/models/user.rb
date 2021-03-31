2972c929f8b633b255c3d3965b2c489f60dab944
mihomatsui <mihomatsui.24@gmail.com>
2021-03-31 12:56:00 +0900

    ゲストログイン処理の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,12 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def self.guest
+    find_or_create_by!(email: "guest@example.com") do |user|
+      user.password = SecureRandom.urlsafe_base64
+      # user.confirmed_at = Time.now # Confirmableを使用している場合は必要
+      # 例えばnameを入力必須としているならば、user.name = "ゲスト"なども必要
+    end
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   resources :posts do
     resource :likes, only: [:create, :destroy]
   end
+
+  devise_scope :user do
+    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+  end
 end
