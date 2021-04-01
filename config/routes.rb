efc87bee42f0767c926be38c607bb906a9d307e6
mihomatsui <mihomatsui.24@gmail.com>
2021-04-01 13:14:45 +0900

    複製されたファイルの削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    registrations: "users/registrations"
-  }
   root "posts#index"
+  devise_for :users, controllers: {
+    registrations: "users/registrations",
+    passwords: "users/passwords"
+  }
+  
   resources :posts do
     resource :likes, only: [:create, :destroy]
   end
