213109b13c2b7c992f5620b999a59129d29d993d
mihomatsui <mihomatsui.24@gmail.com>
2021-04-01 23:42:34 +0900

    passwords_controllerのリダイレクトを修正

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,6 @@ Rails.application.routes.draw do
   end
 
   devise_scope :user do
-    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   end
 end
