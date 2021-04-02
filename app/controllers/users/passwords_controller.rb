25542b23ca13934acb567a076f09f9156d0631f5
mihomatsui <mihomatsui.24@gmail.com>
2021-04-02 20:32:46 +0900

    passwords_controllerを修正

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,9 +1,13 @@
 class Users::PasswordsController < Devise::PasswordsController
   before_action :ensure_normal_user, only: :create
 
+  # ログインを必須にしているので、root_pathを設定するとアカウント登録もしくはログインしてください。という表示になる
+  # パスは通っている
   def ensure_normal_user
     if params[:user][:email].downcase == "guest@example.com"
-      redirect_to new_user_session_path, alert: "ゲストユーザーのパスワード再設定はできません。"
+      # redirect_to new_user_session_path, alert: "ゲストユーザーのパスワード再設定はできません。"だと
+      # ゲストユーザーのパスワード再設定はできません。というアラートが表示される。
+      redirect_to root_path, alert: "ゲストユーザーのパスワード再設定はできません。"
     end
   end
 end
